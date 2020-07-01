ebe39d88f11fc1aa8f57c2b2fda39540f4202b25
Vlad Poletaev <poletaev.vladislav@gmail.com>
2020-07-01 22:09:43 +0300

    Synchronizing the repository with remote with regular interval

--- run.sh
+++ run.sh
@@ -6,3 +6,20 @@ git clone $CLONEPATH wiki
 ./node_modules/.bin/tiddlywiki wiki --listen &
 trap "kill $!" EXIT
 
+function sync_repo() {
+    if [[ -z "$(git status --short)" ]]
+    then
+        return
+    fi
+    git add .
+    git commit -m "Automatic commit at $(date)"
+    git pull --strategy-option=ours --no-edit
+    git push
+}
+
+cd wiki
+
+while true
+do
+    sleep 15s && sync_repo
+done
