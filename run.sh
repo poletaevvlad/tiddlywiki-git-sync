311b8ec03cfae2613fa3ad68582ef7005cbd4bad
Vlad Poletaev <poletaev.vladislav@gmail.com>
2020-07-01 22:21:30 +0300

    Synchronizing the contents of the wiki on script exit

--- run.sh
+++ run.sh
@@ -1,10 +1,19 @@
 #! /bin/sh
 
-test -d "./wiki" && rm -rf wiki
-git clone $CLONEPATH wiki
+function on_terminate() {
+    pid=$1
+    kill -int $pid
 
-./node_modules/.bin/tiddlywiki wiki --listen &
-trap "kill $!" EXIT
+    while $(kill -0 $pid 2> /dev/null)
+    do
+        sleep 1
+    done
+
+    sync_repo
+    cd ..
+    rm -rf wiki
+}
+
 
 function sync_repo() {
     if [[ -z "$(git status --short)" ]]
@@ -16,6 +25,15 @@ function sync_repo() {
     git pull --strategy-option=ours --no-edit
     git push
 }
+
+
+test -d "./wiki" && rm -rf wiki
+git clone $CLONEPATH wiki
+
+./node_modules/.bin/tiddlywiki wiki --listen &
+wikipid=$!
+
+trap "on_terminate $wikipid" EXIT
 
 cd wiki
 
